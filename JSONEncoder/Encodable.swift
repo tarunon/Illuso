2914604cd771ce32007b0bebd0fc5259248c9ffe
tarunon <croissant9603@gmail.com>
2016-01-07 18:36:30 +0900

    add: now supported UInt types.

--- JSONEncoder/Encodable.swift
+++ JSONEncoder/Encodable.swift
@@ -32,5 +32,10 @@ extension Int8: Number {}
 extension Int16: Number {}
 extension Int32: Number {}
 extension Int64: Number {}
+extension UInt: Number {}
+extension UInt8: Number {}
+extension UInt16: Number {}
+extension UInt32: Number {}
+extension UInt64: Number {}
 extension Float: Number {}
 extension Double: Number {}
