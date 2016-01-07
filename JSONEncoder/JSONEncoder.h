85ba25022f0a3b0ad4079782210f9e9bca4ea3d9
tarunon <croissant9603@gmail.com>
2016-01-07 13:56:14 +0900

    Initial Commit

---
+++ JSONEncoder/JSONEncoder.h
@@ -0,0 +1,19 @@
+//
+//  JSONEncoder.h
+//  JSONEncoder
+//
+//  Created by Nobuo Saito on 2016/01/07.
+//  Copyright © 2016年 tarunon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONEncoder.
+FOUNDATION_EXPORT double JSONEncoderVersionNumber;
+
+//! Project version string for JSONEncoder.
+FOUNDATION_EXPORT const unsigned char JSONEncoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONEncoder/PublicHeader.h>
+
+
